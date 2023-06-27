407670b8c8f722cc3461d3bd3f4ad3f51b4dffbb
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-06-27 15:31:05 +0700

    change tax code format

--- AxonsERP.Repository/TaxRateControlRepository.cs
+++ AxonsERP.Repository/TaxRateControlRepository.cs
@@ -31,6 +31,9 @@ namespace AxonsERP.Repository
                                                             G.DESC2 as desc2
                                                             FROM TAX_RATE_CONTROL T, GENERAL_DESC G
                                                             WHERE T.TAX_CODE = G.GDCODE");
+            foreach(var element in result) {
+                element.taxCode = element.taxCode.Substring(5, element.taxCode.Length - 5);
+            }                                                
             return result;
         }
         public TaxRateControl GetSingleTaxRateControl(string taxCode, DateTime effectiveDate) 
@@ -48,6 +51,8 @@ namespace AxonsERP.Repository
                                                                         FROM TAX_RATE_CONTROL T, GENERAL_DESC G
                                                                         WHERE TAX_CODE = :taxCode AND EFFECTIVE_DATE = :effectiveDate AND T.TAX_CODE = G.GDCODE", 
                                                                         new { taxCode, effectiveDate });
+            
+            result.taxCode = result.taxCode.Substring(5, result.taxCode.Length - 5);
             return result;
         }
     }
