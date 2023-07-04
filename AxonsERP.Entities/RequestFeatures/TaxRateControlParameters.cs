86d4d102cfa6acc08bc748e663dfca5f31c00806
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-07-04 14:49:45 +0700

    change property name

--- AxonsERP.Entities/RequestFeatures/TaxRateControlParameters.cs
+++ AxonsERP.Entities/RequestFeatures/TaxRateControlParameters.cs
@@ -10,14 +10,14 @@ namespace AxonsERP.Entities.RequestFeatures
 
     public class TaxRateControlForColumnSearchFilter
     {
-        public string? taxCode { get; set; } = string.Empty;
+        public string? tax_Code { get; set; } = string.Empty;
         public string? owner { get; set; } = string.Empty;
         public float rate { get; set; }
     }
 
     public class TaxRateControlForColumnSearchTerm
     {
-        public string? taxCode { get; set; } = string.Empty;
+        public string? tax_Code { get; set; } = string.Empty;
         public string? owner { get; set; } = string.Empty;
         public float rate { get; set; }
     }
