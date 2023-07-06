801d5b674ad68e4b5c096ab75ac134e6e4e1e99c
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-07-06 17:07:01 +0700

    done get all bill collection dates endpoint

--- AxonsERP.Entities/Models/BillCollectionDateToReturn.cs
+++ AxonsERP.Entities/Models/BillCollectionDateToReturn.cs
@@ -7,7 +7,7 @@ namespace AxonsERP.Entities.Models
         public string? customerCode { get; set; }
         public string? nameLocal { get; set; }
         public string? nameEng { get; set; }
-        public string? department { get; set; }
+        public string? departmentCode { get; set; }
         public string? departmentDesc1 { get; set; }
         public string? departmentDesc2 { get; set; }
         public string? billColCalculate { get; set; }
--- AxonsERP.Repository/BillCollectionDateRepository.cs
+++ AxonsERP.Repository/BillCollectionDateRepository.cs
@@ -19,7 +19,18 @@ namespace AxonsERP.Repository
         
         public IEnumerable<BillCollectionDateToReturn> GetAllBillCollectionDate()
         {
-            return null;
+            var billCollectionDateList = Connection.Query<BillCollectionDateToReturn>(@"SELECT DISTINCT B.CUSTOMER_CODE as customerCode,
+                                                                                        C.NAME_LOCAL as nameLocal,
+                                                                                        C.NAME_ENG as nameEng,
+                                                                                        B.DEPARTMENT as departmentCode,
+                                                                                        G1.DESC1 as departmentDesc1,
+                                                                                        G1.DESC2 as departmentDesc2,
+                                                                                        B.BILL_COL_CALCULATE as billColCalculate,
+                                                                                        G2.DESC1 as billColCalculateDesc1,
+                                                                                        G2.DESC2 as billColCalculateDesc2
+                                                                                        FROM BILL_COL_INFO B, CV_DESC C, GENERAL_DESC G1, GENERAL_DESC G2
+                                                                                        WHERE G1.GDCODE = B.DEPARTMENT AND C.CV_CODE = B.CUSTOMER_CODE AND G2.GDCODE = B.BILL_COL_CALCULATE");
+            return billCollectionDateList;
         }
         public BillCollectionDateToReturn GetCompanyBillCollectionDate(BillCollectionDateForSingle billCollectionDateForSingle)
         {
