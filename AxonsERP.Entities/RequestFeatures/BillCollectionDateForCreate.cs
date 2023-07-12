602438efe750e105b00196dc438560b1965d72ce
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-07-12 15:06:37 +0700

    added default values to dates

--- AxonsERP.Entities/RequestFeatures/BillCollectionDateForCreate.cs
+++ AxonsERP.Entities/RequestFeatures/BillCollectionDateForCreate.cs
@@ -7,10 +7,54 @@ namespace AxonsERP.Entities.Models
         public string? customerCode { get; set; }
         public string? departmentCode { get; set; }
         public string? billColCalculate { get; set; }
-        public int startDate { get; set; }
-        public int endDate { get; set; }
-        public int weekNo { get; set; }
-        public int dayOfWeek { get; set; }
+        public int startDate 
+        { 
+            get
+            {
+                return _startDate;
+            }
+            set
+            {
+                _startDate = value;
+            } 
+        }
+        public int endDate
+        { 
+            get
+            {
+                return _endDate;
+            }
+            set
+            {
+                _endDate = value;
+            } 
+        }
+        public int weekNo
+        { 
+            get
+            {
+                return _weekNo;
+            }
+            set
+            {
+                _weekNo = value;
+            } 
+        }
+        public int dayOfWeek
+        { 
+            get
+            {
+                return _dayOfWeek;
+            }
+            set
+            {
+                _dayOfWeek = value;
+            } 
+        }
         public string? owner { get; set; }
+        private int _startDate = 0;
+        private int _endDate = 0;
+        private int _weekNo = 0;
+        private int _dayOfWeek = 0;
     }
 }
