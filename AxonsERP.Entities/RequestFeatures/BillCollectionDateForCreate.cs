d8f9b4fcd251030b090847765f51b9d51b2a8979
Punn Chunwimaleung <khunpunn2546@gmail.com>
2023-07-14 11:09:29 +0700

    fixed wrong namespace from models to request features

--- AxonsERP.Entities/RequestFeatures/BillCollectionDateForCreate.cs
+++ AxonsERP.Entities/RequestFeatures/BillCollectionDateForCreate.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace AxonsERP.Entities.Models
+namespace AxonsERP.Entities.RequestFeatures
 {
     public class BillCollectionDateForCreate
     {
